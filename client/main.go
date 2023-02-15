bf69f0e7f531abaecac88abf37614183a104677a
Slava Senchenko <sencslav@amazon.com>
2023-02-14 17:02:23 -0800

    brush client up

--- client/main.go
+++ client/main.go
@@ -3,41 +3,59 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"time"
 )
 
-type gqlRequest struct{ 
+type gqlRequest struct {
 	Query string `json:"query"`
 }
 
 func main() {
-	saveNote :=
-		`mutation {
-			saveNote(content: "some note", NoteId: "1", title: "1st note") {
-				title,
-				content
-			}
-		}
-        `
-	saveNoteJson, err := json.Marshal(gqlRequest{saveNote})
+	const kUrl string = "https://fa6oyqp3mbc4jbzyc3k6yaciqq.appsync-api.us-west-2.amazonaws.com/graphql"
+	const kApiKey string = "da2-se5ropd4gncc3jxchyzk5oqt74"
+	response, err := getNote(kUrl, kApiKey)
 	if err != nil {
 		log.Fatal(err)
 	}
-	request, err := http.NewRequest(
-		"POST",
-		"https://fa6oyqp3mbc4jbzyc3k6yaciqq.appsync-api.us-west-2.amazonaws.com/graphql",
-		bytes.NewBuffer(saveNoteJson))
-	request.Header.Add("x-api-key", "da2-se5ropd4gncc3jxchyzk5oqt74")
-	client := &http.Client{Timeout: 10 * time.Second}
-	response, err := client.Do(request)
 	defer response.Body.Close()
-	if err != nil {
-		fmt.Printf("The HTTP request failed with error %s\n", err)
-	}
 	data, _ := io.ReadAll(response.Body)
-	fmt.Println(string(data))
+	log.Println(string(data))
+}
+
+func saveNote(url string, apiKey string) (*http.Response, error) {
+	const saveNote string = `mutation {
+		saveNote(content: "some note", NoteId: "2", title: "1st note") {
+			title,
+			content
+		}
+	}`
+	return makeRequest(saveNote, url, apiKey)
+}
+
+func getNote(url string, apiKey string) (*http.Response, error) {
+	const getNote string = `query {
+		getNote(NoteId: "2") {
+			title
+			content
+		}
+	}`
+	return makeRequest(getNote, url, apiKey)
+}
+
+func makeRequest(graphQl string, url string, apiKey string) (response *http.Response, err error) {
+	json, err := json.Marshal(gqlRequest{graphQl})
+	if err != nil {
+		return
+	}
+	request, err := http.NewRequest("POST", url, bytes.NewBuffer(json))
+	if err != nil {
+		return
+	}
+	request.Header.Add("x-api-key", apiKey)
+	client := &http.Client{Timeout: 10 * time.Second}
+	response, err = client.Do(request)
+	return
 }
